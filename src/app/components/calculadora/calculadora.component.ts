98a66363f2e8da542a80de85b47ab823786725b8
Lucas <lucas_ssilva@outlook.com>
2020-10-13 16:18:02 -0300

    first commit

---
+++ src/app/components/calculadora/calculadora.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-calculadora',
+  templateUrl: './calculadora.component.html',
+  styleUrls: ['./calculadora.component.scss']
+})
+export class CalculadoraComponent implements OnInit {
+
+  input = new FormControl('');
+
+  constructor(private toastr: ToastrService) { }
+
+  ngOnInit(): void {
+    this.input.setValue(localStorage.getItem('lastResult'));
+  }
+
+  insert(num) {
+    let current = this.input.value;
+    this.input.setValue(current += num);
+  }
+
+  c() {
+    this.input.setValue('');
+  }
+
+  equal() {
+    let exp = this.input.value;
+
+    if(exp) {
+      this.input.setValue(eval(exp));
+        localStorage.setItem('lastResult', this.input.value)
+  } else {
+      this.toastr.error('Digite um valor!', 'Erro!');
+    }
+  }
+
+  back() {
+    let exp = this.input.value;
+    this.input.setValue(exp.substring(0, exp.length-1));
+  }
+}
